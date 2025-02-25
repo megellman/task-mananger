103ef7721b9e7a0df8187706b9b33d3f21e61296
Megan Ellman <meganlellman@gmail.com>
2025-02-25 09:33:39 -0800

    modal provides info on how to use the site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 let currentDate = $('#current-date');
 
 // currentDate variable updates every second
-setInterval(function(){
+setInterval(function () {
     currentDate.text(dayjs().format('MMM D, YYYY [at] hh:mm:ss a'))
 }, 1000);
 
 // jqueryui datepicker function for task input form
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+$(function () {
+    $("#datepicker").datepicker();
+});
+
