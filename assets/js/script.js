560d7dff4272e71f3c38e21e28c64abbece26eef
Megan Ellman <meganlellman@gmail.com>
2025-02-24 16:15:08 -0800

    added task input form

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ let currentDate = $('#current-date');
 setInterval(function(){
     currentDate.text(dayjs().format('MMM D, YYYY [at] hh:mm:ss a'))
 }, 1000);
+
+// jqueryui datepicker function for task input form
+$( function() {
+    $( "#datepicker" ).datepicker();
+  } );
