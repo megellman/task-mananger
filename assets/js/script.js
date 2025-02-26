bbdebae8bffc73135921280bea352d2e7a398828
Megan Ellman <meganlellman@gmail.com>
2025-02-26 09:57:58 -0800

    tasks reloaded on refresh created with corresponding color to priority;

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,7 @@ taskForm.on("submit", function (e) {
             $('<td>').text(formData[x]).appendTo(newRow);
         }
     }
-    newRow.append($('<button type="button" class="row-btn-close" aria-label="Close">Delete</button>'));
+    newRow.append($('<button type="button" class="row-btn-close btn-sm bg-secondary rounded" data-toggle="button" aria-label="Close">Delete</button>'));
     newRow.appendTo(tableBody);
 
     // if no storage data yet, then save form data as storage data
@@ -82,9 +82,21 @@ $(document).ready(function () {
         for (let i = 0; i < storageData.length; i++) {
             let newRow = $('<tr>');
             for (let x in storageData[i]) {
-                $('<td>').text(storageData[i][x]).appendTo(newRow);
+                if(storageData[i][x] === '1'){
+                    $('<td>').text('High').appendTo(newRow);
+                    newRow.addClass('bg-danger');
+                } else if(storageData[i][x] === '2'){
+                    $('<td>').text('Medium').appendTo(newRow);
+                    newRow.addClass('bg-warning');
+                } else if(storageData[i][x] === '3'){
+                    $('<td>').text('Low').appendTo(newRow);
+                    newRow.addClass('bg-primary');
+                } else {
+                    $('<td>').text(storageData[i][x]).appendTo(newRow);
+                }
             }
-            newRow.append($('<button type="button" class="row-btn-close" aria-label="Close">Delete</button>'));
+            
+            newRow.append($('<button type="button" class="row-btn-close btn-sm bg-secondary rounded" data-toggle="button" aria-label="Close">Delete</button>'));
             newRow.appendTo(tableBody);
         }
     }
